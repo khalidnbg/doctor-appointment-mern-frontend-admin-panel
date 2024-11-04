24874555efd239706c8ee57ba5cf0f742284c8b3
khalidnbg <khalidnabgaoui4@gmail.com>
2024-11-04 13:07:26 +0100

    setup project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+import AdminContextProvider from "./context/AdminContext";
+import DoctorContextProvider from "./context/DoctorContext";
+import AppContextProvider from "./context/AppContext";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AdminContextProvider>
+      <DoctorContextProvider>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </DoctorContextProvider>
+    </AdminContextProvider>
+  </BrowserRouter>
+);
---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { assets } from "../assets/assets";
+
+const Login = () => {
+  const [state, setState] = useState("Admin");
+
+  return (
+    <form>
+      <div>
+        <p>
+          <span>{state}</span> Login
+        </p>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
